4e016705f69eeb31c82e38f54ddee425a5689e2f
Gaurav Sharma <gsharma81999@gmail.com>
2020-04-11 12:41:50 +0530

    some changes

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import os
 
 MODEL_PATH = os.environ.get("MODEL_PATH")
@@ -8,14 +11,19 @@ MODEL = os.environ.get("MODEL")
 if not SAMPLE_TYPE in ["image", "video", "webcam"]:
     raise ValueError(f"sample type should be `image`, `video` or `webcam` but got {SAMPLE_TYPE}")
 
-
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 from keras import preprocessing
 
 
-haarcascade = cv2.CascadeClassifier(MODEL_PATH + "haarcascade.xml")
+cascade_lassifiers = [
+    "haarcascade_frontalface",
+    "lbpcascade_frontalface",
+    "lbpcascade_profileface",
+]
+
+haarcascade = cv2.CascadeClassifier(MODEL_PATH + f"{cascade_lassifiers[1]}.xml")
 model = load_model(MODEL_PATH + MODEL)
 
 emotions = ['happy', 'sad', 'neutral']
@@ -38,24 +46,26 @@ def get_emotion(img_arr):
 
 if SAMPLE_TYPE == "image":
     img = cv2.imread(TEST_SAMPLE_PATH + "img/happy2.jpg", 0)
+    img = cv2.equalizeHist(img) 
     img_arr = preprocess_img(img)
     predicted_emotion = get_emotion(img_arr)
-
     cv2.imshow(f"person is looking {predicted_emotion}", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 else:
     iswebcam = True
     if SAMPLE_TYPE == "webcam":
         vidcap=cv2.VideoCapture(0)
     else:
         iswebcam = False
-        vidcap=cv2.VideoCapture(TEST_SAMPLE_PATH + 'videos/video3.mp4')
+        vidcap=cv2.VideoCapture(TEST_SAMPLE_PATH + 'videos/video1.mp4')
+        # out = cv2.VideoWriter('output.mp4', -1, 20.0, (640,480))
 
     while True:
         status, frame = vidcap.read()
         if not status:
-            continue
+            break
 
         if iswebcam:
             frame=cv2.flip(frame,1,0)
@@ -68,15 +78,18 @@ else:
             x, y, w, h = face
             cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), thickness=7)
 
-            img = gray_frame[y:y+w, x:x+h]        
+            img = gray_frame[y:y+w, x:x+h]
+            img = cv2.equalizeHist(img)
             img_arr = preprocess_img(img)
 
             predicted_emotion = get_emotion(img_arr)
 
             cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
 
-        resized_img = cv2.resize(frame, (1000, 700))
+        resized_img = cv2.resize(frame, (600, 500))
         cv2.imshow('Facial Emotion Recognition',resized_img)
+        # if not iswebcam:
+        #     out.write(resized_img)
 
         if cv2.waitKey(10) == ord('q'):
             break
